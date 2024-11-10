d9bce7548adc64655ca730012c70702932b223a4
nguyenthanhlinh22 <thanhlinhnguyen22032004@gmail.com>
2024-11-10 20:54:54 +0700

    show list

--- src/main/java/org/example/profilecase5/Service/UserService.java
+++ src/main/java/org/example/profilecase5/Service/UserService.java
@@ -5,16 +5,26 @@ import org.example.profilecase5.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List; // Import for List
+
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
+    // Fetch a user by ID
     public User getUserById(int userId) {
         return userRepository.findById(userId).orElse(null);
     }
+
+    // Update or save a user
     public void updateUser(User user) {
         userRepository.save(user);
+    }
+
+    // Fetch all users
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
