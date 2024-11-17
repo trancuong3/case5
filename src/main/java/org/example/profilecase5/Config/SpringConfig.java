cb165685be29b2630ebfefc4b209cd8441726ca6
xuanson1 <hustle50k@gmail.com>
2024-11-18 02:37:31 +0700

    Login GG

---
+++ src/main/java/org/example/profilecase5/Config/SpringConfig.java
@@ -0,0 +1,27 @@
+package org.example.profilecase5.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(registry->{
+                    registry.requestMatchers("/").permitAll();
+                    registry.anyRequest().authenticated();
+                })
+                .oauth2Login(oauth2Login -> {
+                    oauth2Login
+                            .loginPage("/login")
+                            .successHandler((request, response, authentication) -> response.sendRedirect("/userProfile"));
+                })
+//                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
