227b65fb274b28d30a4d80403253f3bfa53d013e
Your Namechipkiet <you@example hoccuakiet@gmail.com>
2024-11-20 19:55:15 +0700

    Update home interface

--- src/main/java/org/example/profilecase5/Controller/HomeController.java
+++ src/main/java/org/example/profilecase5/Controller/HomeController.java
@@ -1,14 +1,25 @@
 package org.example.profilecase5.Controller;
 
+import org.example.profilecase5.Model.User;
+import org.example.profilecase5.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping("")
-    public String getHome() {
+    public String getHome(Model model, Authentication auth) {
+        String username = auth.getName();
+        User user =   userService.getCurrentUser();
+        model.addAttribute("user", user);
         return "home/home";
     }
 }
